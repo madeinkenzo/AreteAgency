22d704122696aa9c89f2c591b4e9cdd7e8da936f
madeinkenzo <georgiospetkoslb@icloud.com>
2024-12-24 15:00:31 +0100

    CHecking some stuff

---
+++ AreteAgency/myscripts.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to detect mobile devices
+    function isMobileDevice() {
+      return /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+  
+    // Check and restrict access
+    if (!isMobileDevice()) {
+      document.body.innerHTML = '<h1> Sorry, this website is only accessible on mobile devices. </h1>';
+    }
+  });
